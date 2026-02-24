474c6e05ea32817d3e944f53ffb7bcb4a4944b07
Tahina <dombrowski.tahina47@gmail.com>
2026-02-24 14:14:00 +0100

    TP1 fini

--- src/jeu/Sabot.java
+++ src/jeu/Sabot.java
@@ -1,14 +1,26 @@
 package jeu;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import cartes.Carte;
 
-public class Sabot {
-	int nbCartes;
-	Carte[] cartes;
+public class Sabot implements Iterable<Carte>{
+	private int nbCartes;
+	private int capacite;
+	private Carte[] pioche;
+	private int nombreOperations = 0;
 	
-	public Sabot(int nbCartes, Carte[] cartes) {
-		this.nbCartes = nbCartes;
-		this.cartes = cartes;
+	public Iterator<Carte> iterator() { 
+		return new Iterateur(); 
+	}
+	
+	
+	public Sabot(Carte[] pioche) {
+		this.pioche = pioche;
+		this.nbCartes = pioche.length;
+		this.capacite = pioche.length;
 	}
 	
 	public boolean estVide() {
@@ -16,6 +28,68 @@ public class Sabot {
 	}
 	
 	public void ajouterCarte(Carte carte) {
+		if (nbCartes > capacite)
+			throw new IllegalStateException();
+		else {
+			pioche[nbCartes] = carte;
+			nbCartes++;
+			nombreOperations++;
+		}
+	}
+	
+	public Carte piocher() {
+		if (estVide()) {
+			throw new IllegalStateException();
+		}
+		Iterateur iterateur = new Iterateur();
+		Carte cartePiochee = null;
+		if (iterateur.hasNext())
+			cartePiochee = iterateur.next();
+		iterateur.remove();
+		nbCartes--;
+		return cartePiochee;
+	}
+	
+	private class Iterateur implements Iterator<Carte> {
+		private int indiceIterateur = 0;
+		private boolean nextEffectue = false;
+		private int nombreOperationsReference = nombreOperations;
 		
+		@Override
+		public boolean hasNext() {
+			return (indiceIterateur < nbCartes);
+		}
+		
+		@Override
+		public Carte next() {
+			verificationConcurrence();
+			if (hasNext()) {
+				Carte nextCarte = pioche[indiceIterateur];
+				nextEffectue = true;
+				indiceIterateur++;
+				return nextCarte;
+			} else
+				throw new NoSuchElementException();
+		}
+		
+		@Override
+		public void remove() {
+			verificationConcurrence();
+			if (nbCartes < 1 || !nextEffectue)
+				throw new IllegalStateException();
+			for (int i = indiceIterateur - 1; i < nbCartes - 1; i++) {
+				pioche[i] = pioche[i+1];
+			}
+			nextEffectue = false;
+			indiceIterateur--;
+			nbCartes--;
+			nombreOperations++; 
+			nombreOperationsReference++;
+		}
+		
+		private void verificationConcurrence(){
+			if (nombreOperations != nombreOperationsReference)
+				throw new ConcurrentModificationException();
+		}
 	}
 }
--- src/testsFonctionnels/TestSabot.java
+++ src/testsFonctionnels/TestSabot.java
@@ -1,10 +1,40 @@
 package testsFonctionnels;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import cartes.Botte;
+import cartes.Carte;
+import cartes.JeuDeCartes;
+import jeu.Sabot;
+
 public class TestSabot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JeuDeCartes nouveauJeu = new JeuDeCartes();
+		Sabot sabotJeu1 = new Sabot(nouveauJeu.donnerCartes());
+		Carte cartePiochee;
+		
+		while (!(sabotJeu1.estVide())) {
+				cartePiochee = sabotJeu1.piocher();
+				System.out.println("je pioche " + cartePiochee.toString());
+		}
+		
+		Sabot sabotJeu2 = new Sabot(nouveauJeu.donnerCartes());
+		cartePiochee = sabotJeu2.piocher();
+		sabotJeu2.ajouterCarte(new Botte(cartes.Type.ACCIDENT));
+		try {
+			for (Iterator<Carte> iterator = sabotJeu2.iterator(); iterator.hasNext();) {
+				System.out.println("je pioche " + iterator.next());
+				iterator.remove();
+				cartePiochee = sabotJeu2.piocher();
+				
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.println("Trop ou pas assez de cartes dans la pioche.");
+		} catch (ConcurrentModificationException e) {
+			System.out.println("Plusieurs joueurs piochent en même temps.");
+		}
 	}
 
 }
